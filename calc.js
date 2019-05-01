73f2afac9b3a0bd904067a76dae9c24eb616c474
Brendan D. Gilroy <bdgilroy@gmail.com>
2019-05-01 17:14:04 -0400

    refactored according to part 2 blog post

--- calc.js
+++ calc.js
@@ -1,9 +1,10 @@
 // SO 24464404
-let readline = require('readline');
+const readline = require('readline');
 
-let rl = readline.createInterface({
+const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    prompt: 'calc> '
 });
 
 
@@ -13,15 +14,26 @@ let rl = readline.createInterface({
 // there is no more input left for lexical analysis
 const INTEGER = 'INTEGER';
 const PLUS = 'PLUS';
+const MINUS = 'MINUS';
 const EOF = 'EOF';
 
 function Token(type, value) {
     this.type = type;
     this.value = value;
+    this.toString = function(){
+        return 'Token(`${this.type}, ${this.value})`';
+    };
 }
 
 function isNumeric(n) {
     return !isNaN(n - parseFloat(n));
+}
+
+String.prototype.isSpace = function(char) {
+    if (' \t\n\r\v'.indexOf(char) === -1) {
+        return false;
+    }
+    return true;
 }
 
 function Interpreter(text) {
@@ -29,45 +41,63 @@ function Interpreter(text) {
     this.pos = 0;
 
     this.currentToken = null;
+    this.currentChar = this.text.slice(this.pos, this.pos+1);
 
     this.error = function() {
         throw 'Error parsing output';
     }
+        
+    this.advance = function() {
+        this.pos += 1;
+        if (this.pos > this.text.length - 1) {
+            this.currentChar = null;
+        } else {
+            this.currentChar = text.slice(this.pos, this.pos+1);
+        }
+    }
+    this.skipWhitespace = function() {
+        while (this.currentChar != null && this.currentChar.isSpace()) {
+            this.advance();
+        }
+    }
+    this.integer = function() {
+        result = '';
+        while (this.currentChar != null && isNumeric(this.currentChar)) {
+            result += this.currentChar;
+            this.advance();
+        }
+        return parseInt(result);
+    }   
+
     this.getNextToken = function() {
-        text = this.text;
+        while (this.currentChar !== null) {
 
-        if (this.pos > text.length-1) {
-            return new Token(EOF, null);
-        }
-
-        currentChar = text.slice(this.pos, this.pos+1);
-
-        if (isNumeric(currentChar)) {
-            integer = "";
-            while (isNumeric(currentChar)) {
-                integer += currentChar;
-                this.pos += 1;
-                currentChar = text.slice(this.pos, this.pos+1);
+            if (this.currentChar.isSpace()) {
+                this.skipWhitespace();
+                continue;
             }
-            token = new Token(INTEGER, parseInt(integer));
-            return token;
-        }
 
-        if (currentChar === ' ') {
-            this.pos += 1;
-            this.getNextToken();
-        }
+            if (isNumeric(this.currentChar)) {
+                return new Token(INTEGER, this.integer());
+            }
 
-        if (currentChar === '+') {
-            token = new Token(PLUS, currentChar);
-            this.pos += 1;
-            return token;
-        }
+            if (this.currentChar === '+') {
+                this.advance();
+                return new Token(PLUS, this.currentChar);
+            }
 
-        this.error();
+            if (this.currentChar === '-') {
+                this.advance();
+                return new Token(MINUS, this.currentChar);
+            }
+
+            this.error();
+        }
+        return new Token(EOF, null);
     }
 
     this.eat = function(tokenType) {
+        
         if (this.currentToken.type === tokenType) { 
             this.currentToken = this.getNextToken();
         } else {
@@ -82,32 +112,35 @@ function Interpreter(text) {
         this.eat(INTEGER);
 
         op = this.currentToken;
-        this.eat(PLUS);
+        if (op.type === PLUS) {
+            this.eat(PLUS);
+        } else {
+            this.eat(MINUS);
+        }
 
         right = this.currentToken;
         this.eat(INTEGER);
 
-        result = left.value + right.value;
+        if (op.type === PLUS) {
+            result = left.value + right.value;
+        } else {
+            result = left.value - right.value;
+        }
         return result;
     }
 }
 
 const main = function(){
-    try {
-        //https://stackoverflow.com/questions/k/how-to-readline-infinitely-in-node-js
-        rl.setPrompt('calc> ', 6);
-        rl.on('line', function(text) {
-            interpreter = new Interpreter(text.trim());
-            result = interpreter.expr();
-            console.log(result);
-            rl.prompt();
-        }).on('close', function() {
-            process.exit(0);
-        });
+    //https://stackoverflow.com/questions/k/how-to-readline-infinitely-in-node-js
+    rl.on('line', function(text) {
+        interpreter = new Interpreter(text.trim());
+        result = interpreter.expr();
+        console.log(result);
         rl.prompt();
-    } catch(e){
-        console.log(e);
-    }
+    }).on('close', function() {
+        process.exit(0);
+    });
+    rl.prompt();
 }
 
 main();
